9e7df79f1e8742abc591aee42acae776979b581a
Michael Ebert <michael.ebert@gmail.com>
2016-02-24 12:12:20 +0100

    Rename common bundle output

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
+var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.bundle.js');
 
 module.exports = {
   context:  path.resolve(__dirname, 'client'),
