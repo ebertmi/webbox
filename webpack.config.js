1ac0264adf8b10c48e407deaec55c57b58c6ca89
Johannes Henninger <johannes@henninger.io>
2016-02-25 13:50:29 +0100

    Config & cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -25,16 +25,12 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015']
+          presets: ['es2015', 'react']
         }
       },
       {
         test: /\.sass$/,
         loaders: ['style', 'css', 'sass']
-      },
-      {
-        test: /\.jade$/,
-        loaders: ['jade']
       }
     ]
   },
