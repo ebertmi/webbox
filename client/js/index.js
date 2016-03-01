18c3b3061b285275bc4334b0d88e5ff3a5f9ffa7
Michael Ebert <michael.ebert@gmail.com>
2016-03-01 12:33:16 +0100

    Fixes header and footer, adds reg form

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,14 @@
 require('scss/index');
 
 var Terminal = require('term.js');
+var Dropzone = require("dropzone");
+
+// Dropzone config for media upload
+Dropzone.options.mediaupload = {
+  paramName: "imageFile", // The name that will be used to transfer the file
+  init:function () {
+    this.on('success', function (result) {
+      console.log(JSON.parse(result.xhr.response));
+    });
+  }
+};
