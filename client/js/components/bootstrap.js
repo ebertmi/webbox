ba7cf43969fccaa7413dd6e1aa2cf6e81cc03c56
Johannes Henninger <johannes@henninger.io>
2016-03-09 14:58:35 +0100

    Improve bootstrap components

--- client/js/components/bootstrap.js
+++ client/js/components/bootstrap.js
@@ -14,33 +14,34 @@ export class Input extends React.Component {
         'form-control-label': this.grid
       });
 
-      return <label htmlFor={this.props.Id} className={classes}>{this.props.label}</label>;
+      return <label htmlFor={this.props.id} className={classes}>{this.props.label}</label>;
     }
   }
 
   renderInput() {
     switch (this.props.type) {
-    case 'textarea':
-      return <textarea {...this.props} className="form-control" ref={input => this.input = input}/>;
-    case 'select':
-      return (
-        <select {...this.props} className='form-control c-select' ref={input => this.input = input}>
-          {this.props.children}
-        </select>
-      );
-    default:
-      let input = <input {...this.props} className="form-control" ref={input => this.input = input}/>;
-
-      if (this.isCheckbox()) {
+      case 'textarea':
+        return <textarea {...this.props} className="form-control" ref={input => this.input = input}/>;
+      case 'select':
         return (
+          <select {...this.props} className='form-control c-select' ref={input => this.input = input}>
+            {this.props.children}
+          </select>
+        );
+      default: {
+        let input = <input {...this.props} className="form-control" ref={input => this.input = input}/>;
+
+        if (this.isCheckbox()) {
+          return (
             <label className="c-input c-checkbox">
               {input}
               <span className="c-indicator"/>
               {this.grid ? null : this.props.label}
             </label>
-        );
-      } else {
-        return input;
+          );
+        } else {
+          return input;
+        }
       }
     }
   }
@@ -59,6 +60,7 @@ export class Input extends React.Component {
     });
 
     let offsets;
+
     if (this.grid && !this.props.label && this.props.labelClassName) {
       offsets = this.props.labelClassName.split(' ').map(c => {
         let match = c.match(/(col-\w{2})-(\d*)/);
@@ -85,7 +87,9 @@ export class Input extends React.Component {
 
 Input.propTypes = {
   type: React.PropTypes.string.isRequired,
+  children: React.PropTypes.node,
   label: React.PropTypes.node,
+  className: React.PropTypes.string,
   labelClassName: React.PropTypes.string,
   id: React.PropTypes.string,
   muted: React.PropTypes.string,
@@ -94,7 +98,6 @@ Input.propTypes = {
 
 export class InputButton extends Input {
   renderLabel() {
-
   }
 
   renderInput() {
