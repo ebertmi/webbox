fb7e9a739bebf5164d7e2c37a0ef9979198dba47
Michael Ebert <michael.ebert@gmail.com>
2017-01-07 23:07:11 +0100

    Fixes terminal resize behavior and deps updates

--- webbox.js
+++ webbox.js
@@ -13,7 +13,6 @@ import CatboxMemory from 'catbox-memory';
 import CatboxRedis from 'catbox-redis';
 import Log from './lib/models/log';
 import HapiPM2 from './lib/util/hapi-pm2';
-import { hapiRequestingJson, isRequestingJson } from './lib/util/hapiUtils';
 
 import isString from 'lodash/isString';
 
