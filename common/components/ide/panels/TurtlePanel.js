ff0e882afd7f81f18958f381c56812aee462ab0e
Michael Ebert <michael.ebert@gmail.com>
2017-05-11 13:33:15 +0200

    Fixes too small turtle canvas which causes hidden lines

--- common/components/ide/panels/TurtlePanel.js
+++ common/components/ide/panels/TurtlePanel.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ResizeDetector from '../../ResizeDetector';
+import round from 'lodash/round';
 
 export default class TurtlePanel extends React.Component {
   constructor(props) {
     super(props);
 
     this.onRef = this.onRef.bind(this);
+    this.onResize = this.onResize.bind(this);
 
     this.state = {
       zoomFactor: 1
@@ -24,9 +26,9 @@ export default class TurtlePanel extends React.Component {
   /**
    * Gets called when the turtle container has been rendered.
    * The ref is the actual DOMNode and allows us to append existing nodes.
-   * 
-   * @param {DOMNode} ref 
-   * 
+   *
+   * @param {DOMNode} ref - dom node reference
+   * @returns {void}
    * @memberOf TurtlePanel
    */
   onRef(ref) {
@@ -38,50 +40,11 @@ export default class TurtlePanel extends React.Component {
   }
 
   /**
-   * Calculates a zooming factor so that the canvas can be displayed within the panel.
-   * 
-   * @param {any} width 
-   * @param {any} height 
-   * @returns 
-   * 
-   * @memberOf TurtlePanel
-   */
-  recalculateZoomFactor(width, height) {
-    if (width == null || height == null) {
-      return;
-    }
-
-    let canvasWidth = this.props.item.canvas.width;
-    let canvasHeight = this.props.item.canvas.height;
-
-    if (canvasWidth == null || canvasHeight == null) {
-      return;
-    }
-
-    let wFactor = width / canvasWidth;
-    let hFactor = height / canvasHeight;
-    let factor;
-
-    // if we need to choose the smallest factor if we need to reduce the size
-    if (Math.min(wFactor, hFactor) < 1) {
-      factor = Math.max(Math.min(wFactor, hFactor), 0.3);
-    } else {
-      // otherwise choose the max factor
-      factor = Math.min(Math.max(wFactor, hFactor), 2);
-    }
-
-    // Triggers rerendering if factor changes
-    this.setState({
-      zoomFactor: factor
-    });
-  }
-
-  /**
    * Event handler for dimension changes of the panel.
-   * 
-   * @param {any} width 
-   * @param {any} height 
-   * 
+   *
+   * @param {any} width - width of the panel
+   * @param {any} height - height of the panel
+   * @returns {void}
    * @memberOf TurtlePanel
    */
   onResize(width, height) {
@@ -90,9 +53,9 @@ export default class TurtlePanel extends React.Component {
 
   /**
    * Returns the width and height of the turtle container parent (panel).
-   * 
+   *
    * @returns {Array|null} an Array [width, height] or null if information is not available
-   * 
+   *
    * @memberOf TurtlePanel
    */
   getContainerSize() {
@@ -102,8 +65,49 @@ export default class TurtlePanel extends React.Component {
         this.turtleContainer.parentElement.offsetHeight,
       ];
     }
-  
+
     return null;
+  }
+
+  /**
+   * Calculates a zooming factor so that the canvas can be displayed within the panel.
+   *
+   * @param {any} width - width of the panel
+   * @param {any} height - height of the panel
+   * @returns {void}
+   *
+   * @memberOf TurtlePanel
+   */
+  recalculateZoomFactor(width, height) {
+    if (width == null || height == null) {
+      return;
+    }
+
+    const canvasWidth = this.props.item.canvas.width;
+    const canvasHeight = this.props.item.canvas.height;
+
+    if (canvasWidth == null || canvasHeight == null) {
+      return;
+    }
+
+    const wFactor = width / canvasWidth;
+    const hFactor = height / canvasHeight;
+    let factor;
+
+    // if we need to choose the smallest factor if we need to reduce the size
+    if (Math.min(wFactor, hFactor) < 1) {
+      factor = Math.max(Math.min(wFactor, hFactor), 0.5);
+    } else {
+      // otherwise choose the max factor
+      factor = Math.min(Math.max(wFactor, hFactor), 2);
+    }
+
+    factor = round(factor, 2);
+
+    // Triggers rerendering if factor changes
+    this.setState({
+      zoomFactor: factor
+    });
   }
 
   render() {
@@ -111,7 +115,7 @@ export default class TurtlePanel extends React.Component {
       <div className="turtle-panel">
         <h1>Turtle-Ausgabe</h1>
         <div ref={this.onRef} style={{ zoom: this.state.zoomFactor }}></div>
-        <ResizeDetector handleWidth handleHeight onResize={this.onResize.bind(this)} />
+        <ResizeDetector handleWidth handleHeight onResize={this.onResize} />
       </div>
     );
   }
